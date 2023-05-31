e29204b15bfe7f987e1b62171d57275a462b9ba2
vermataruna <tarunaverma792@gmail.com>
2023-05-31 23:55:15 +0530

    add: inertia & vue js setup

---
+++ resources/js/Pages/Users/Index.vue
@@ -0,0 +1,10 @@
+<script setup>
+
+</script>
+
+<template>
+  <Layout>
+    <h1>Welcome</h1>
+    <p>Hello welcome to your first Inertia app!</p>
+  </Layout>
+</template>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,15 @@
-import './bootstrap';
+import { createApp, h } from 'vue';
+import { createInertiaApp } from '@inertiajs/vue3';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+ 
+createInertiaApp({
+    resolve: name => {
+        const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
+        return pages[`./Pages/${name}.vue`]
+      },
+  setup({ el, App, props, plugin }) {
+    return createApp({ render: () => h(App, props) })
+      .use(plugin)
+      .mount(el)
+  },
+});
