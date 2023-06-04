98aaa510fc798d373aa002fbd6620345c1b69d2a
vermataruna <tarunaverma792@gmail.com>
2023-06-04 14:49:54 +0530

    add: navlink component

---
+++ resources/js/Shared/NavLink.vue
@@ -0,0 +1,21 @@
+<template>
+    <Link
+    class="text-blue-500 hover:underline"
+    :class="{'font-bold': active}"
+    >
+        <slot></slot>
+    </Link>
+</template>
+
+<script>
+import { Link } from '@inertiajs/vue3'
+
+export default {
+    props: {
+        active: Boolean
+    },
+    components: {
+        Link
+    }
+}
+</script>
