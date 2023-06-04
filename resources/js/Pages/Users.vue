17b4c9409d7d566e74ddcb89461e59270a3e0a9e
vermataruna <tarunaverma792@gmail.com>
2023-06-04 13:48:09 +0530

    add: preserve scroll

--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -1,14 +1,27 @@
 <template>
   <h1 class="text-4xl font-bold">Users</h1>
   <Nav/>
+
+  <div style="margin-top: 800px">
+    <p>The current time is {{ time }}.</p>
+    <Link href="/users" class="text-blue-500" preserve-scroll>
+      Refresh
+    </Link>
+  </div>
+
 </template>
 
 <script>
 import Nav from '../Shared/Nav.vue';
+import { Link } from '@inertiajs/vue3';
 
 export default {
   components: {
-      Nav
+      Nav,
+      Link
+  },
+  props: {
+    time: String
   }
 }
 
--- routes/web.php
+++ routes/web.php
@@ -8,8 +8,9 @@ Route::get('/', function () {
 });
 
 Route::get('/users', function () {
-    sleep(3);
-    return Inertia::render('Users');
+    return Inertia::render('Users', [
+        'time' => now()->toTimeString()
+    ]);
 });
 
 Route::get('/settings', function () {
